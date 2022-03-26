18b9e96786e0624ecb68a25377acbc273e9b42e6
fernando.junior <fernando.junior@informata.com.br>
2022-03-26 15:14:01 -0300

    :sparkles: mudando o componente material para primeng nas telas de contatos

--- src/app/shared/modules/prime-ng-module.ts
+++ src/app/shared/modules/prime-ng-module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 
-import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 
 
 @NgModule({
   exports: [
-
+    CardModule,
+    InputTextModule,
+    ButtonModule,
   ],
 })
 export class PrimeNGModules { }
